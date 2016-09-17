a32d1cd3a50e6894246c1216de74e3237dc53db6
Tarun Gattu <gttarun2.0@gmail.com>
2016-09-17 18:26:08 -0500

    configuring database on heroku

--- happs/settings.py
+++ happs/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,7 +134,7 @@ STATICFILES_DIRS = [
 	'/var/www/static/',
 ]
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
