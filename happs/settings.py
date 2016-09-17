ba7f062e60f8e4bf751d54342c1f33657b69d0af
Tarun Gattu <gttarun2.0@gmail.com>
2016-09-17 18:34:24 -0500

    configuring heroku db

--- happs/settings.py
+++ happs/settings.py
@@ -135,14 +135,3 @@ STATICFILES_DIRS = [
 ]
 
 DATABASES['default'] =  dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-	from .local_settings import *
-except ImportError:
-	pass
