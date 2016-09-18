3a9138eb2d5ca07c6cb5928ffafdf95537b95fd2
Tarun Gattu <gttarun2.0@gmail.com>
2016-09-17 19:28:32 -0500

    db changes- removed location for right now

--- events/models.py
+++ events/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class UserEvents(models.Model):
 	event_name = models.CharField(max_length=255)
 	time = models.DateTimeField()
-	longitude = models.FloatField()
-	latitude = models.FloatField()
+	# longitude = models.FloatField(default=1)
+	# latitude = models.FloatField(default=1)
 
 	def __str__(self):
 		return self.event_name
