dd1562afd9b3df2f64403eae3d64771162896ed0
Tarun Gattu <gttarun2.0@gmail.com>
2016-09-24 18:25:50 -0500

    POST worksss- or it should, or i quit

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status, permissions
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route, list_route
 from rest_framework.response import Response
 from .models import UserEvents
 from django.http import HttpResponse
@@ -7,18 +7,6 @@ from .serializers import EventSerializer
 
 def index(request):
 	return HttpResponse("Hello world!")
-
-@api_view(['GET', 'POST'])
-def post(request):
-	if request.method == 'GET':
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	elif request.method == 'POST':
-		serializer = EventSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def event(request, pk):
 	queryset = UserEvents.objects.get(pk)
@@ -29,3 +17,12 @@ def event(request, pk):
 class EventsViewSet(viewsets.ModelViewSet):
 	queryset = UserEvents.objects.all()
 	serializer_class = EventSerializer
+
+	@detail_route(methods=['post'])
+	def addEvent(self, request):
+		serializer = EventSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
