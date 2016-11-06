d1356b51994426b30e8696b0c1dc488c16ab4969
Tarun Gattu <gttarun2.0@gmail.com>
2016-11-06 17:09:30 -0600

    added image retrieval endpoint (which works) —HELLAAYAA

--- upload/views.py
+++ upload/views.py
@@ -1,5 +1,7 @@
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import FileUpload
 from .serializers import FileUploadSerializer
@@ -15,5 +17,11 @@ class FileUploadViewSet(ModelViewSet):
 	serializer_class = FileUploadSerializer
 	parser_classes = (MultiPartParser, FormParser,)
 
-	def perform_create(self, serializer):
+	def create(self, serializer):
 		serializer.save(datafile=self.request.data.get('datafile'))
+
+	def retrieve(self, request, pk):
+		return Response(FileUploadSerializer(FileUpload.objects.get(username=pk)).data)
+
+
+
