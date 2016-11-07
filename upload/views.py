99e5026dc7bbd53d44578867b5e4d36b493668ff
Tarun Gattu <gttarun2.0@gmail.com>
2016-11-07 16:10:40 -0600

    picture retrieval works

--- upload/views.py
+++ upload/views.py
@@ -5,11 +5,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import FileUpload
 from .serializers import FileUploadSerializer
+from django.utils.encoding import smart_str
+from PIL import Image
 
 def get_image(request, image_id):
-    db_image = FileUpload.objects.get(pk=image_id)
-    image = "../" + str(db_image.datafile)
-    return render(request, 'upload/show_image.html', {'image': image})
+	db_image = FileUpload.objects.get(pk=image_id)
+	image = "../" + str(db_image.datafile)
+	return render(request, 'upload/show_image.html', {'image': image})
 
 class FileUploadViewSet(ModelViewSet):
 	
@@ -21,7 +23,18 @@ class FileUploadViewSet(ModelViewSet):
 		serializer.save(datafile=self.request.data.get('datafile'))
 
 	def retrieve(self, request, pk):
-		return Response(FileUploadSerializer(FileUpload.objects.filter(username=pk), many=True).data)
+		# return Response(FileUploadSerializer(FileUpload.objects.filter(username=pk), many=True).data)
+		
+		db_image = FileUpload.objects.get(username=pk)
+		# image = str(db_image.datafile)
+		# response = HttpResponse(image, content_type='image/jpeg')
+		# response['Content-Disposition'] = 'attachment; filename=%s' % smart_str("seven.jpg")
+		# response['X-Sendfile'] = smart_str(image)
+		# return response
 
-
-
+		im = str(db_image.datafile)
+		image = Image.open(im)
+		response = HttpResponse(content_type='application/force-download')
+		response['Content-Disposition'] = 'attachment; filename=%s' % smart_str("seven.jpg")
+		image.save(response, "jpeg")
+		return response
